4a9c7b5d155ca0d078f288b63fc17e888fd111b6
sayam9901 <sayamkhan9560621912@gmail.com>
2023-09-18 19:02:31 +0530

    change all cost to 100 on click

--- src/components/Expense.js
+++ src/components/Expense.js
@@ -7,11 +7,16 @@ const Expense = (props) => {
     const { data } = props;
 
     const [expense , setExpense] = useState(data)
+    const [title , setTitle] = useState(data[0].cost)
 
     const delteitem = (index) =>{
        const updatedData = [...expense]
        updatedData.splice(index,1);
        setExpense(updatedData)
+    }
+
+    const setValue = (newtitle) =>{
+      setTitle(newtitle)
     }
   return (
     <>
@@ -23,8 +28,9 @@ const Expense = (props) => {
                 <div className='expense-item__description'>
                 <h2>{data.expense}</h2>
                 <h2>{data.location}</h2>
-                <div className='expense-item__price'>{data.cost}</div>
+                <div className='expense-item__price'>{title}</div>
                 <button onClick={()=>delteitem(data.id)}> delete item</button>
+                <button onClick={()=>setValue("Rs 100")}>change expense value</button>
                 </div>
                
             </Card>
