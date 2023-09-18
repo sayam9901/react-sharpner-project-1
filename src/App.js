531e75acaff9656424a9641e97acb2e8907ef794
sayam9901 <sayamkhan9560621912@gmail.com>
2023-09-18 18:27:12 +0530

    making a delete button to make list delte

--- src/App.js
+++ src/App.js
@@ -3,18 +3,21 @@ import './App.css';
 import Expense from './components/Expense';
 const data = [
   {
+    id : 0,
    expense : "food",
    cost : "rs 10",
    date : new Date(2023 ,5 ,12),
    location : "restorant"
   },
   {
+    id:1,
       expense : "petrol",
       cost : "rs 100",
       date : new Date(2023 ,5 ,12),
       location : "bharat petrolium"
      },
      {
+      id : 2,
       expense : "Movies",
       cost : "rs 200",
       date : new Date(2023 ,5 ,12),
--- src/components/Expense.js
+++ src/components/Expense.js
@@ -1,21 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./expense.css"
 import Date from './Date';
 import Card from './Card';
 
 const Expense = (props) => {
     const { data } = props;
+
+    const [expense , setExpense] = useState(data)
+
+    const delteitem = (index) =>{
+       const updatedData = [...expense]
+       updatedData.splice(index,1);
+       setExpense(updatedData)
+    }
   return (
     <>
     <h1>EXPENSE TRACKER</h1>
     {
-        data.map((data)=>(
-            <Card className='expense-item'>
+        expense.map((data)=>(
+            <Card key = {data.id} className='expense-item'>
                 <Date date = {data.date}/>
                 <div className='expense-item__description'>
                 <h2>{data.expense}</h2>
                 <h2>{data.location}</h2>
                 <div className='expense-item__price'>{data.cost}</div>
+                <button onClick={()=>delteitem(data.id)}> delete item</button>
                 </div>
                
             </Card>
